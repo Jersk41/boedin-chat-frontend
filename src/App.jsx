e303dbb8263d607617ac5ddef5cb0728a38b5849
_userNull <ramzprivatexd@gmail.com>
2025-01-19 20:10:30 +0700

    feat: edit modal, menu

--- src/App.jsx
+++ src/App.jsx
@@ -3,13 +3,14 @@ import reactLogo from "./assets/react.svg";
 import viteLogo from "/vite.svg";
 import ChatBox from "./components/ChatBox";
 
-import useDataStore from "./store/Store";
 import Usermodal from "./components/UserModal";
+import EditModal from "./components/EditModal";
 
 export default function App() {
     return (
         <div className="w-full h-screen flex bg-background items-center justify-center">
             <Usermodal />
+            <EditModal />
             <ChatBox />
         </div>
     );
